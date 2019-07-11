5bebb743c17ac59ba57b33bfd4d91a9fddbcc378
mchiszar <52429587+mchiszar@users.noreply.github.com>
2019-07-11 14:05:00 -0500

    lesson 6

--- app/src/main/java/com/example/swoosh3/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh3/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.example.swoosh3
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun nextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+
     }
 }
---
+++ app/src/main/java/com/example/swoosh3/SkillActivity.kt
@@ -0,0 +1,14 @@
+package com.example.swoosh3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+
+
+    }
+}
